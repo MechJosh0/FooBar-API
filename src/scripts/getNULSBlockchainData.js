7d5d311d9bc0263a37b1f45b686e0ce0b32683e6
Josh Welham <josh@aluminati.net>
2019-05-27 16:30:49 +0100

    prepare processing transaction and correctly sleep between each block

--- src/scripts/getNULSBlockchainData.js
+++ src/scripts/getNULSBlockchainData.js
@@ -2,11 +2,37 @@ require('module-alias/register');
 require('dotenv').config();
 
 const axios = require('axios');
+const { forEach } = require('p-iteration');
+const logger = require('@/utils/winston');
 const Database = require('@/models/Database');
+const { sleep } = require('@/utils/debugTools');
+
+const processBlockHeader = async (tr, header) =>
+{
+	delete header.txList;
+
+	// Insert header query...
+};
+
+const processBlockTransactions = async (tr, transactions) =>
+{
+	logger.verbose('Inserting transaction...');
+
+	await forEach(transactions, async (transaction) =>
+	{
+		await sleep(2); // Debug
+		// Insert transaction query...
+		logger.verbose('Transaction inserted');
+	});
+
+	logger.verbose('Inserting transactions completed');
+};
 
 const run = async () =>
 {
 	const db = new Database();
+	const date = new Date();
+	let sleepSeconds = 10; // This is overwritten
 
 	await db.init();
 
@@ -14,19 +40,53 @@ const run = async () =>
 	{
 		try
 		{
-			console.log(`Connecting to ${process.env.NULS_ENV} node...`);
-			const { data: { success, data } } = await axios(`${process.env.NODE_HOST}/${process.env.NULS_ENV}/block/height/1`);
-
-			if(!success)
+			await db.transaction(db, async (tr) =>
 			{
-				throw new Error(`Request error -> ${data.msg}`);
-			}
+				logger.verbose(`Connecting to ${process.env.NULS_ENV} node...`);
 
-			console.log(data);
+				const latestLocalHeight = await tr.select1('SELECT height FROM blockHeaders ORDER BY id DESC LIMIT 1');
+				const nextLocalHeight = (latestLocalHeight || 0) + 1;
+
+				const { data: { data: { value: latestNetworkHeight } } } = await axios(`${process.env.NODE_HOST}/${process.env.NULS_ENV}/block/newest/height`);
+
+				if(nextLocalHeight > latestNetworkHeight) // Network hasn't confirmed the next block yet
+				{
+					// In theory we should never see this but it's a fallback
+					sleepSeconds = 1;
+					logger.verbose(`We're at the maximum height: ${latestNetworkHeight}`);
+
+					return;
+				}
+
+				logger.verbose(`Getting next height: ${nextLocalHeight}`);
+
+				const { data: { success, data: block } } = await axios(`${process.env.NODE_HOST}/${process.env.NULS_ENV}/block/height/${nextLocalHeight}`);
+
+				if(!success)
+				{
+					throw new Error(`Request error -> ${block.msg}`);
+				}
+
+				logger.verbose(`We found block height: ${block.height}`);
+
+				await processBlockTransactions(tr, block.txList);
+				await processBlockHeader(tr, block);
+
+				if(block.height !== latestNetworkHeight)
+				{
+					sleepSeconds = 0;
+					logger.verbose(`We do not have the max height ${block.height}/${latestNetworkHeight}`);
+				}
+				else
+				{
+					sleepSeconds = 10 - parseInt((`${date.getSeconds()}`)[1] || date.getSeconds(), 10); // Calculate seconds to the next 10 second block in time
+					logger.verbose(`Sleep until the next height: ${sleepSeconds}`);
+				}
+			});
 		}
 		catch(e)
 		{
-			console.warn('Error:', e.message);
+			logger.error(e.message);
 		}
 	};
 
@@ -35,7 +95,7 @@ const run = async () =>
 	// setInterval(() =>
 	// {
 	// 	start();
-	// }, 10000); // Every 5 minutes
+	// }, sleepSeconds * 1000);
 };
 
 run();
